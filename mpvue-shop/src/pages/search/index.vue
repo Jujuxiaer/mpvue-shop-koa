cbf37cd730ce95d934b35748598221a4216aa44c
nameJujuxia <zhihua.ju@iranshao.com>
2020-02-20 10:17:29 +0800

    [add]-订单模块

--- mpvue-shop/src/pages/search/index.vue
+++ mpvue-shop/src/pages/search/index.vue
@@ -40,18 +40,25 @@
       </div>
     </div>
     <!-- 商品列表 -->
-    <div class="goodList">
+    <div class="goodsList" v-if="listData.length!==0">
       <div class="sortnav">
-        <div>综合</div>
-        <div>价格</div>
-        <div>分类</div>
+        <div @click="changeTab(0)" :class="[0 === nowIndex ? 'active' : '']">综合</div>
+        <div @click="changeTab(1)" :class="[1 === nowIndex ? 'active' : '']" class="price">价格</div>
+        <div @click="changeTab(2)" :class="[2 === nowIndex ? 'active' : '']">分类</div>
+      </div>
+      <div class="sortlist">
+        <div @click="goodsDetail(item.id)" class="item" v-for="(item, index) in listData" :key="index">
+          <img :src="item.list_pic_url" alt="">
+          <p class="name">{{item.name}}</p>
+          <p class="price">¥{{item.retail_price}}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { get, post } from '../../utils'
+import { get, post, getStorageOpenid } from '../../utils'
 export default {
   data () {
     return {
@@ -59,18 +66,26 @@ export default {
       openid: '',
       hotData: [],
       historyData: [],
-      tipsData: []
+      tipsData: [],
+      order: '',
+      listData: [],
+      nowIndex: 0
     }
   },
   mounted () {
-    this.openid = wx.getStorageSync('openId') || '';
+    this.openid = getStorageOpenid();
     this.getHotData()
   },
   methods: {
     clearInput() {
       this.words = ''
+      this.listData = []
     },
-    cancel() {},
+    cancel() {
+      wx.navigateBack({
+        delta: 1
+      })
+    },
     async clearHistory() {
       const data = await post('/search/clearhistoryAction', {
         openId: this.openid
@@ -81,7 +96,9 @@ export default {
     },
     inputFocus() {
       // 商品清空
-      
+      this.listData = []
+      // 展示搜索提示信息
+      this.tipsearch()
     },
     async tipsearch() {
       const data = await get('/search/helperaction', {
@@ -96,15 +113,37 @@ export default {
         openId: this.openid,
         keyword: value || this.words
       })
-      // console.log(data)
       // 获取历史数据
       this.getHotData()
+      this.getlistData()
     },
     async getHotData (first) {
       const data = await get('/search/indexaction?openId=' + this.openid)
       this.historyData = data.historyData
       this.hotData = data.hotKeywordList
-      // console.log(data)
+    },
+    async getlistData () {
+      // 获取商品列表
+      const data  = await get('/search/helperaction', {
+        keyword: this.words,
+        order: this.order
+      })
+      this.listData = data.keywords
+      this.tipsData = []
+    },
+    changeTab(index) {
+      this.nowIndex = index
+      if (index === 1) {
+        this.order = this.order == 'asc' ? 'desc' : 'asc'
+      } else {
+        this.order = ''
+      }
+      this.getlistData()
+    },
+    goodsDetail (id) {
+      wx.navigateTo({
+        url: '/pages/goods/main?id=' + id
+      });
     }
   }
 }
